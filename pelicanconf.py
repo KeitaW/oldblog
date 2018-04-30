827be1d51dadb587a5a30489d4bcf8fb0fe6c92d
Keita F. Watanabe <keitaw09@gmail.com>
2018-04-30 18:47:20 +0900

    change theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 AUTHOR = 'KeitaW'
 SITENAME = 'Manekiel'
 SITEURL = ''
-THEME = "pelican-themes/blue-penguin"
+THEME = "pelican-themes/pelican-blue"
 PATH = 'content'
 TIMEZONE = 'Asia/Tokyo'
 DEFAULT_LANG = 'ja'
@@ -27,42 +27,53 @@ AUTHOR_FEED_RSS = None
 #         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
+SOCIAL = (('linkedin', 'https://www.linkedin.com/in/keita-watanabe-b33956141'),
+          ('github', 'https://github.com/keitaw'),
+          ('twitter', 'https://twitter.com/keitaw09'),
+          )
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
+# Pelican-blue settings
+SIDEBAR_DIGEST = 'PhD Student in a laboratory for Computational Neuroscience'
+#FAVICON = 'url-to-favicon'
+DISPLAY_PAGES_ON_MENU = True
+TWITTER_USERNAME = 'keitaw09'
+MENUITEMS = (('Blog', SITEURL),)
+
 # Blue-penguins settings
 # all defaults to True.
-DISPLAY_HEADER = True
-DISPLAY_FOOTER = True
-DISPLAY_HOME   = True
-DISPLAY_MENU   = True
-
-# provided as examples, they make ‘clean’ urls. used by MENU_INTERNAL_PAGES.
-TAGS_URL           = 'tags'
-TAGS_SAVE_AS       = 'tags/index.html'
-AUTHORS_URL        = 'authors'
-AUTHORS_SAVE_AS    = 'authors/index.html'
-CATEGORIES_URL     = 'categories'
-CATEGORIES_SAVE_AS = 'categories/index.html'
-ARCHIVES_URL       = 'archives'
-ARCHIVES_SAVE_AS   = 'archives/index.html'
-
-# use those if you want pelican standard pages to appear in your menu
-MENU_INTERNAL_PAGES = (
-    ('Tags', TAGS_URL, TAGS_SAVE_AS),
-    ('Author', AUTHORS_URL, AUTHORS_SAVE_AS),
-    ('Categories', CATEGORIES_URL, CATEGORIES_SAVE_AS),
-    ('Archives', ARCHIVES_URL, ARCHIVES_SAVE_AS),
-)
-# additional menu items
-MENUITEMS = (
-    ('GitHub', 'https://github.com/'),
-    ('Twitter', 'https://github.com/'),
-    ('Linkedin', 'https://www.kernel.org/'),
-)
+#DISPLAY_HEADER = True
+#DISPLAY_FOOTER = True
+#DISPLAY_HOME   = True
+#DISPLAY_MENU   = True
+#
+## provided as examples, they make ‘clean’ urls. used by MENU_INTERNAL_PAGES.
+#TAGS_URL           = 'tags'
+#TAGS_SAVE_AS       = 'tags/index.html'
+#AUTHORS_URL        = 'authors'
+#AUTHORS_SAVE_AS    = 'authors/index.html'
+#CATEGORIES_URL     = 'categories'
+#CATEGORIES_SAVE_AS = 'categories/index.html'
+#ARCHIVES_URL       = 'archives'
+#ARCHIVES_SAVE_AS   = 'archives/index.html'
+#
+## use those if you want pelican standard pages to appear in your menu
+#MENU_INTERNAL_PAGES = (
+#    ('Tags', TAGS_URL, TAGS_SAVE_AS),
+#    ('Author', AUTHORS_URL, AUTHORS_SAVE_AS),
+#    ('Categories', CATEGORIES_URL, CATEGORIES_SAVE_AS),
+#    ('Archives', ARCHIVES_URL, ARCHIVES_SAVE_AS),
+#)
+## additional menu items
+#MENUITEMS = (
+#    ('GitHub', 'https://github.com/'),
+#    ('Twitter', 'https://github.com/'),
+#    ('Linkedin', 'https://www.kernel.org/'),
+#)
 
